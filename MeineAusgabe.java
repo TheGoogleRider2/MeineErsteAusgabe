212e8dc4b5eb3265881397874d706ec865395d4b
TheGoogleRider2 <87247880+TheGoogleRider2@users.noreply.github.com>
2021-09-16 23:37:46 +0200

    Add files via upload

---
+++ MeineAusgabe.java
@@ -0,0 +1,15 @@
+/**
+ * @name: (MeinErsteAusgabe)
+ * @description:
+ * It's a Hello, world program :)
+ *
+ * @author (Vincent-Oniichan)
+ * @version (1.0)
+ * @date (16/9/2021)
+ */
+public class MeineAusgabe{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hi Senpai");
+    }
+}
