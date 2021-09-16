b53093b066b4211fb38dfe5a02028caa6aab77c0
TheGoogleRider2 <vbrabandt2@gmail.com>
2021-09-17 01:16:09 +0200

    Update MeineAusgabe.java
    - Updated Name & Date
    - A some cleaning

--- MeineAusgabe.java
+++ MeineAusgabe.java
@@ -3,13 +3,17 @@
  * @description:
  * It's a Hello, world program :)
  *
- * @author (Vincent-Oniichan)
- * @version (1.0)
- * @date (16/9/2021)
+ * @author (TheGoogleRider)
+ * @version (1.2)
+ * @date (17/9/2021)
  */
+
 public class MeineAusgabe{
     public static void main(String[] args) 
     {
-        System.out.println("Hi Senpai");
+        System.out.println("Hi Senpai"); //Don't ask why i chose Hi Senpai
     }
 }
+
+
+
