67f2eaf2f75b3eecd9fd3571513cd65d269fcd81
TheGoogleRider2 <vbrabandt2@gmail.com>
2021-09-26 15:57:32 +0200

    Final Update for Java version

--- MeineAusgabe.java
+++ MeineAusgabe.java
@@ -1,20 +1,55 @@
 /**
- * @name: (MeinErsteAusgabe)
+ * @name: (MeinErsteAusgabe_main)
  * @description:
- * It's a Hello, world program :)
- *
+ * It's a Hello, world program with some new commands we learned in class
+ * 
+ * I guess this is the final version.
+ * We just had to write some stuff down so yeah.
+ * 
+ * 
  * @author (TheGoogleRider)
- * @version (1.2)
- * @date (17/9/2021)
+ * @version (2.0)
+ * @date (26/09/2021)
  */
 
-public class MeineAusgabe{
-    public static void main(String[] args) 
-    {
-        System.out.println("Hi Senpai"); //Don't ask why i chose Hi Senpai
+
+
+public class MeineAusgabe
+{
+    public class MeinAusgabe{
+        public static void main(String[] args) 
+        {
+            System.out.println("Hi Senpai");
+            System.out.println();
+
+            double number; 
+            number = 17.25;
+            System.out.println(number * 4);
+            System.out.println();
+
+            char letter;
+            letter = 'a';
+            System.out.println(letter);
+            System.out.println();
+            
+            String text;
+            text = "Oppai";
+            System.out.println(text);
+            System.out.println();
+
+            int zahl;
+            zahl = 69;
+            System.out.println(zahl);
+            System.out.println(zahl);
+            System.out.println();
+            
+            String PP;
+            PP = "Made for school by TheGoogleRider";
+            System.out.println(PP);
+            System.out.println();
+        }
     }
 }
 
-
-//Yamete kudasai
+//Yamete kudasai!!
 //By TheGoogleRider
