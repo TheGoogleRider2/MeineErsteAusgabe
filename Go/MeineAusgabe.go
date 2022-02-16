67f7a9463c4b34f927e520b179a343e45ba5bcf4
TheGoogleRider2 <vbrabandt2@gmail.com>
2022-02-16 20:57:06 +0100

    Clean Up Update
    - Clean some stuff up
    - Swift main code working
    - Go added

--- Go/MeineAusgabe.go
+++ Go/MeineAusgabe.go
@@ -1,5 +1,18 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("hello world")
+	fmt.Println("Hi Senpai")
+	fmt.Println()
+
+	number := 17.4225
+	fmt.Println(number)
+
+	text := "Oppai"
+	fmt.Println(text)
+	fmt.Println()
+
+	fmt.Println("By TheGoogleRider")
 }
+//By TheGoogleRider
---
+++ Swift/MeineAusgabe.swift
@@ -0,0 +1,21 @@
+/** name: MeineErsteAusgabe
+ *  description:
+ *  Swift Cheat Sheet I guess. 
+ *  
+ *  author: @TheGoogleRider
+ *  version: 1.0
+ *  date: 16/02/2022
+**/
+
+import Foundation
+
+print("Hi Senpai")
+print()
+var number = 17.4225; print(number)
+
+let text = "Oppai"; print(text)
+
+var zahl = 8; print(zahl); print()
+
+let PP = "By TheGoogleRider"; print(PP)
+//By TheGoogleRider
