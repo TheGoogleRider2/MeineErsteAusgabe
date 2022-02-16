82448f89f169bdd7a31a0be6564492f2f6caf16d
TheGoogleRider2 <vbrabandt2@gmail.com>
2022-02-16 20:35:40 +0100

    Update MeineAusgabe_lite.swift

--- Swift/MeineAusgabe_lite.swift
+++ Swift/MeineAusgabe_lite.swift
@@ -1,3 +1,12 @@
+/** name: MeineErsteAusgabe
+ *  description:
+ *  Swift Cheat Sheet I guess. (Lite)
+ *  
+ *  author: @TheGoogleRider
+ *  version: 1.0
+ *  date: 16/02/2022
+**/
+
 print("Hi Senpai")
 
 //By TheGoogleRider
