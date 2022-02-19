fc66f00ac1b1ede5edc7b1efaf7e37c4725c3a58
TheGoogleRider2 <vbrabandt2@gmail.com>
2022-02-19 16:48:25 +0100

    C++

---
+++ C++/MeineAusgabe/MeineAusgabe.cpp
@@ -0,0 +1,33 @@
+/**
+ * name: MeinErsteAusgabe
+ * description:
+ * Trying out C++
+ * 
+ * author: @TheGoogleRider
+ * version: (1.0)
+ * date: 19/02/2022
+ */
+
+#include <iostream>
+
+int main()
+{
+    //This is to print something. (The \n is for new lines)
+    std::cout << "Hello world! \n\n";
+    
+    double number = 17.4225;
+    std:: cout << number; "\n\n";
+
+    char letter = 'a';
+    std:: cout << letter; "\n\n";
+
+    std:: cout << "Oppai \n\n";
+
+    int zahl = 69;
+    std:: cout << zahl; "\n\n";
+
+    std:: cout << "Made for school by TheGoogleRider";
+
+
+    return 0;
+}
